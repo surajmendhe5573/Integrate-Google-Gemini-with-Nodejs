7a35f0508b8e499a183287c326021c5021268aab
surajmendhe5573 <surajmendhe470@gmail.com>
2024-12-04 21:47:34 +0530

    google gemini api

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-// require('dotenv').config();
-
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-// const prompt = "Explain how AI works";
-
-// const result = await model.generateContent(prompt);
-// console.log(result.response.text());
-
-
+const express= require('express');
+const app= express();
+const bodyParser= require('body-parser');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config();
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.json());
+
+const port= process.env.PORT || 4000
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain Backend deceloper ?";
+// const prompt = "Explain Backend deceloper ?";
 
-const generate= async()=>{
+const generate= async(prompt)=>{
     try {
         const result = await model.generateContent(prompt);
         console.log(result.response.text());
+        return result.response.text();
     } catch (error) {
         console.log(error);
         
     }
 }
 
-generate();
+// generate();
+
+app.get('/api/content', async(req, res)=>{
+    try {
+         const data= req.body.question;
+         const result= await generate(data);
+         res.send({
+            "result": result
+         })
+         
+    } catch (error) {
+     console.log(error);
+     
+    }
+ });
+
+app.listen(port, ()=>{
+    console.log(`server is running on http://localhost:${port}`);
+})
